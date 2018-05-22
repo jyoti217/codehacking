9d40285c8db4999fcc7f6dcbf1fee200b9e23084
jyoti <jyotichd21@gmail.com>
2018-05-23 01:14:33 +0530

    Added edit user funnstionality

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -44,7 +45,12 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        $input = $request->all();
+        if(!empty(trim($request->password))){
+            $input = $request->except(['password']);
+        }else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
         //
         if($file = $request->file('photo_id')){
             $name = time().$file->getClientOriginalName();
@@ -53,9 +59,9 @@ class AdminUsersController extends Controller
             $input['photo_id'] = $photo->id;
 
         }
-        $input['password'] = bcrypt($request->password);
+
         User::create($input);
-        redirect('admin/users');
+        return redirect('admin/users');
         //dd($request->all());
 
     }
@@ -81,7 +87,9 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
-        return view('admin.users.edit');
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name', 'id')->all();
+        return view('admin.users.edit', compact('user', 'roles'));
     }
 
     /**
@@ -91,9 +99,24 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserEditRequest $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+        if(!empty(trim($request->password))){
+            $input = $request->except(['password']);
+        }else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+        if($file = $request->file('photo_id')){
+            $name = $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+        $user->update($input);
+        return redirect('/admin/users');
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,6 +7,7 @@
         <thead>
             <tr>
                 <th>ID</th>
+                <th>Photo</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Role</th>
@@ -21,7 +22,8 @@
             @foreach($users as $user)
                 <tr>
                     <td>{{$user->id}}</td>
-                    <td>{{$user->name}}</td>
+                    <td><img height="40" src="{{$user->photo?$user->photo->file:'http://placehold.it/400x400'}}" alt=""></td>
+                    <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
                     <td>{{$user->email}}</td>
                     <td>{{$user->role->name}}</td>
                     <td>{{($user->is_active)? 'Yes': 'No'}}</td>
